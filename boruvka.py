c1550cc0f08c61cca0e47055a24e21a054ba5dc9
justsamuel <samuel.oosterholt20001004@gmail.com>
2021-12-13 13:28:13 +0100

    last attempt at boruvka affinity, still does not work.

--- boruvka.py
+++ boruvka.py
@@ -50,7 +50,14 @@ def boruvka_mrc(dataset):
 
 def find_best_neighbours_mpc(p_components, p_dm):
     global sc
-    best_neighbours = sc.parallelize(list(p_components.values())).map(lambda x: (x.get_cheapest_neighbour(p_dm)))
+
+    leader_mapping = {}
+    for components in p_components:
+        for vertex in p_components[components].vertices:
+            leader_mapping[vertex] = components
+    print(leader_mapping)
+
+    best_neighbours = sc.parallelize(list(p_components.values())).map(lambda x: (x.get_cheapest_neighbour(leader_mapping, p_dm)))
 
     best_neighbours = best_neighbours.collect()
     mapping = {}
@@ -65,24 +72,42 @@ def contract(v, p_components, p_dm, mapping):
     while v not in S:
         S.append(v)
         v = mapping[v]
-    return S
+    return min(S), S
+
+
+def keep_largest(ar, arr):
+    if len(ar) > len(arr):
+        return ar
+    return arr
+
+
+def create_paths(a):
+    leader_index = a[1].index(a[0])
+    if leader_index == len(a[1]) - 1:
+        return [a[1][::-1]]
+    if leader_index > 0:
+        return [[a[0]] + a[1][:leader_index][::-1], a[1][leader_index:]]
+    return [a[1]]
 
 
 def contraction_mpc(p_components, p_dm, mapping):
     global sc
     deletions = sc.parallelize(list(p_components.values())).map(
-        lambda x: contract(x.leader, p_components, p_dm, mapping))
-    print(deletions.collect())
+        lambda x: contract(x.leader, p_components, p_dm, mapping)).reduceByKey(lambda a, b: keep_largest(a, b))
+    chains = deletions.collect()
+    chains = sc.parallelize(chains).map(lambda a: (a[0], create_paths(a))).collect()
+    merged = sc.parallelize(chains).map(lambda a: p_components[a[0]].merge_chains(a[1], p_components, p_dm))
+    merged = merged.collect()
 
-    to_remove = []
-    for component in p_components:
-        if p_components[component].leader == -1:
-            to_remove.append(component)
-
-    for component in to_remove:
-        remove_component(p_components, component)
-
-    print(len(p_components.keys()))
+    for returns in merged:
+        leader = returns[0]
+        if leader in p_components:
+            component = p_components[leader]
+            component.vertices = set(returns[1][1])
+            component.edges = set(returns[1][2])
+            for to_remove in returns[1][0]:
+                if to_remove != leader:
+                    remove_component(p_components, to_remove)
 
 
 if __name__ == '__main__':
