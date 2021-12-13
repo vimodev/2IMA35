aa1f3730da9c88a5045fad761cafc20e5405e37e
justsamuel <samuel.oosterholt20001004@gmail.com>
2021-12-13 10:24:14 +0100

    PySpark stopped working :)

--- boruvka.py
+++ boruvka.py
@@ -2,63 +2,11 @@ import math
 import random
 from datetime import datetime
 
+from pyspark import SparkConf, SparkContext
+
+from component import create_component, remove_component
 from prim import prim
 from helpers import create_distance_matrix, get_clustering_data, plot_mst
-
-
-class Component:
-    def __init__(self, i):
-        self.leader = i
-        self.vertices = {i}
-        self.edges = set()
-
-    def set_leader(self, leader):
-        self.leader = leader
-
-    def __str__(self):
-        return f"[Component #{self.leader}]"
-
-    def get_cheapest_neighbour(self, p_dm):
-        best_cost = math.inf
-        edge = None
-        for vertex in self.vertices:
-            for target in range(len(p_dm[0])):
-                if target not in self.vertices:
-                    cost = p_dm[vertex][target]
-                    if best_cost > cost:
-                        best_cost = cost
-                        edge = (vertex, target)
-        return edge
-
-    def merge_with_best(self, p_components, p_dm):
-        best_edge = self.get_cheapest_neighbour(p_dm)
-
-        s = best_edge[0]
-        t = best_edge[1]
-        self.edges.add((s, t, p_dm[s][t]))
-
-        for component in p_components:
-            if t in p_components[component].vertices:
-                other_component = p_components[component]
-                self.vertices = self.vertices.union(other_component.vertices)
-                self.edges = self.edges.union(other_component.edges)
-                remove_component(p_components, other_component.leader)
-                return
-
-
-def create_component(p_components, vertex):
-    p_components[vertex] = Component(vertex)
-
-
-def remove_component(p_components, vertex):
-    p_components.pop(vertex, None)
-
-
-def change_component_leader(p_components, old_vertex, new_vertex):
-    temp = p_components[old_vertex]
-    temp.set_leader(new_vertex)
-    remove_component(p_components, old_vertex)
-    p_components[new_vertex] = temp
 
 
 def boruvka(dataset):
@@ -82,8 +30,63 @@ def boruvka(dataset):
     # print(components)
 
 
+def boruvka_mrc(dataset):
+    conf = SparkConf().setAppName('MST_Algorithm').set("spark.driver.bindAddress", "127.0.0.1").setMaster(
+        "local[4]").set("spark.yarn.appMasterEnv.JAVA_HOME", "/usr/lib/jvm/java-8-openjdk")
+    global sc
+    sc = SparkContext(conf=conf, pyFiles=['./component.py'])
+
+    dm, E, size, vertex_coordinates = create_distance_matrix(dataset[0][0])
+    components = {}
+    for vertex in range(len(dataset[0][0])):
+        create_component(components, vertex)
+    while len(components.keys()) >= 2:
+        mapping = find_best_neighbours_mpc(components, dm)
+        contraction_mpc(components, dm, mapping)
+
+    mst = list(list(components.values())[0].edges)
+    plot_mst(dataset[0][0], mst, False, False)
+
+
+def find_best_neighbours_mpc(p_components, p_dm):
+    global sc
+    best_neighbours = sc.parallelize(list(p_components.values())).map(lambda x: (x.get_cheapest_neighbour(p_dm)))
+
+    best_neighbours = best_neighbours.collect()
+    mapping = {}
+    for edge in best_neighbours:
+        mapping[edge[0]] = edge[1]
+
+    return mapping
+
+
+def contract(v, p_components, p_dm, mapping):
+    S = []
+    while v not in S:
+        S.append(v)
+        v = mapping[v]
+    return S
+
+
+def contraction_mpc(p_components, p_dm, mapping):
+    global sc
+    deletions = sc.parallelize(list(p_components.values())).map(
+        lambda x: contract(x.leader, p_components, p_dm, mapping))
+    print(deletions.collect())
+
+    to_remove = []
+    for component in p_components:
+        if p_components[component].leader == -1:
+            to_remove.append(component)
+
+    for component in to_remove:
+        remove_component(p_components, component)
+
+    print(len(p_components.keys()))
+
+
 if __name__ == '__main__':
     datasets = get_clustering_data()
-    for dataset in datasets:
-        prim(dataset)
-        boruvka(dataset)
+    boruvka_mrc(datasets[0])
+    # for dataset in datasets:
+    #     boruvka_mrc(dataset)
